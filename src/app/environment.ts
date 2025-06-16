ea00a2006ee934436880f144f4702b2cbd4e391e
BledanIT <bledan@gmail.com>
2025-06-16 19:47:16 +0200

    edit: repairing location page (attempt #1)

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: false,
     googleApiKey: 'AIzaSyBXodAhjGrnhVm7aSCaFx6I4M7EYOU_S7o',
     placeId: "ChIJe3AcjHttiEcRzlAbdpFCwY4",
-    googleOAuthId: "60460424095-r3or1ipjlq6nn7imse8pnjkonqtv5d0o.apps.googleusercontent.com"
+    googleOAuthId: "60460424095-r3or1ipjlq6nn7imse8pnjkonqtv5d0o.apps.googleusercontent.com",
+    mapsAPILink: "https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBXodAhjGrnhVm7aSCaFx6I4M7EYOU_S7o&libraries=places"
 };
