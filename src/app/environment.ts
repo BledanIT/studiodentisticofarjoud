3c35eb2a1ebf49c04afc4f294b49e3bb1bb4eefb
BledanIT <bledan@gmail.com>
2025-06-18 00:24:53 +0200

    removed secret leak

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -3,5 +3,14 @@ export const environment = {
     googleApiKey: 'AIzaSyBXodAhjGrnhVm7aSCaFx6I4M7EYOU_S7o',
     placeId: "ChIJe3AcjHttiEcRzlAbdpFCwY4",
     googleOAuthId: "60460424095-r3or1ipjlq6nn7imse8pnjkonqtv5d0o.apps.googleusercontent.com",
-    mapsAPILink: "https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBXodAhjGrnhVm7aSCaFx6I4M7EYOU_S7o&libraries=places"
+    mapsAPILink: "https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBXodAhjGrnhVm7aSCaFx6I4M7EYOU_S7o&libraries=places",
+    firebaseConfig: {
+        apiKey: "AIzaSyDOyujh-DnR8qsBKS1Gbs3ZRcj2My24O6o",
+        authDomain: "bledan-studio-dentistico.firebaseapp.com",
+        projectId: "bledan-studio-dentistico",
+        storageBucket: "bledan-studio-dentistico.firebasestorage.app",
+        messagingSenderId: "60460424095",
+        appId: "1:60460424095:web:8ad149552352f1f40b3baa",
+        measurementId: "G-9KT50PY9VY"
+    },
 };
