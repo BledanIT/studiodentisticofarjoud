87bc0b1489316e082402777b3c0024eba2aa8a8e
BledanIT <bledan@gmail.com>
2025-06-18 00:24:53 +0200

    removed secret leak

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -20,16 +20,21 @@ export default {
   {
     "renderMode": 2,
     "route": "/studiodentisticofarjoud/appointment"
+  },
+  {
+    "renderMode": 2,
+    "route": "/studiodentisticofarjoud/privacy"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 32188, hash: '300fe199c12b196eaadb5f76764c12fc589c52b03b311a757f608291db017667', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 25794, hash: 'abde7255f753c577920c9cd8f1a6ed448748e261e3a9132333ad93eb1fe3b399', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'where/index.html': {size: 127755, hash: '21eef339778b692a69a9962b8835e7a5acc218b51fc45121b45c7e00c74d307c', text: () => import('./assets-chunks/where_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 118274, hash: '2c888fa810991b0c1e295e83bc0692b1d017b9fe72f2ffda9bf932b6098d1217', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'pricing/index.html': {size: 123030, hash: 'd1c41c4e82b544e50dee4a8c8fe038051721c4119b1dcecc0d189b65cf91b007', text: () => import('./assets-chunks/pricing_index_html.mjs').then(m => m.default)},
-    'appointment/index.html': {size: 220565, hash: '763ff6acc025b06dde6ae9385da8bea014b409383b2a93c9807015c903a73936', text: () => import('./assets-chunks/appointment_index_html.mjs').then(m => m.default)},
-    'styles-XXCUDO7D.css': {size: 7043, hash: 'dMkBV/bw5fY', text: () => import('./assets-chunks/styles-XXCUDO7D_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 32196, hash: '7a8fac8b74e046183428a0030f82fd7aa452129ae9b5ddfb8db22ef29bae8198', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 25852, hash: 'b70527a9059e63044f53ffb1c778550f065a62e9873fc8a5f22f2aba48d9f915', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'where/index.html': {size: 130284, hash: '9407745e31ee7e3e24da36710346c5b9804dec86fdab9ae10470cbacc332c915', text: () => import('./assets-chunks/where_index_html.mjs').then(m => m.default)},
+    'privacy/index.html': {size: 108779, hash: 'c2b5f96a9b19f074f31fde3fb6311b0041d84547bd0b2f8fe29d663e0d2701e7', text: () => import('./assets-chunks/privacy_index_html.mjs').then(m => m.default)},
+    'pricing/index.html': {size: 126183, hash: 'feb5e3d375f26381d6e13ec5c5393f79a552aa4b46a12eaa3fbc223bced19887', text: () => import('./assets-chunks/pricing_index_html.mjs').then(m => m.default)},
+    'appointment/index.html': {size: 226800, hash: '830ae45f048c939cff5ec0c528a2d2018edaf017c559372c0f127440bbc53e8d', text: () => import('./assets-chunks/appointment_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 121237, hash: 'e3399f41e11c5c51908cf11a21ecf3655ba9476b79184d02c75ef3497508464d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-65IWZ6W2.css': {size: 6993, hash: '+A9W7XfYYT8', text: () => import('./assets-chunks/styles-65IWZ6W2_css.mjs').then(m => m.default)}
   },
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,16 +31,7 @@ export class AppComponent {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const firebaseConfig = {
-
-  apiKey: "AIzaSyDOyujh-DnR8qsBKS1Gbs3ZRcj2My24O6o",
-  authDomain: "bledan-studio-dentistico.firebaseapp.com",
-  projectId: "bledan-studio-dentistico",
-  storageBucket: "bledan-studio-dentistico.firebasestorage.app",
-  messagingSenderId: "60460424095",
-  appId: "1:60460424095:web:8ad149552352f1f40b3baa",
-  measurementId: "G-9KT50PY9VY"
-};
+//const firebaseConfig;
 
 // Initialize Firebase
 //const app = initializeApp(firebaseConfig);
