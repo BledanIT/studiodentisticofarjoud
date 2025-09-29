938c9254327dfc94adcd5d6941a42ff24959b882
BledanIT <bledan@gmail.com>
2025-09-30 00:03:24 +0200

    firestore integration part 2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,10 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
-import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from './environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,11 +18,10 @@ export const appConfig: ApplicationConfig = {
       ),
     ),
     provideClientHydration(withEventReplay()),
-    provideMomentDateAdapter(), provideFirebaseApp(() => initializeApp({ projectId: "bledan-studio-dentistico", appId: "1:60460424095:web:8ad149552352f1f40b3baa", storageBucket: "bledan-studio-dentistico.firebasestorage.app", apiKey: "AIzaSyDOyujh-DnR8qsBKS1Gbs3ZRcj2My24O6o", authDomain: "bledan-studio-dentistico.firebaseapp.com", messagingSenderId: "60460424095", measurementId: "G-9KT50PY9VY" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
-    /*importProvidersFrom([
-      provideFirebaseApp(() => initializeApp(firebaseConfig)),
-      provideFirestore(() => getFirestore())
-    ])*/
+    provideMomentDateAdapter(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ]
 };
 
